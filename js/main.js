de681b5842421f8d34f4147262924d1c16331e1e
Pedro Rivero <perm17@gmail.com>
2024-10-28 08:00:02 -0300

    commit 01

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const productos = [
         id: "combo01",
         nombre: "Combo Full",
         precio: 12500,
-        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at tincidunt arcu. Ut quis dolor leo. Sed id malesuada massa. Suspendisse aliquet faucibus commodo.",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         imagenDestacada: "",
         categoria: {
             id: "combos",
@@ -16,7 +16,7 @@ const productos = [
         id: "combo02",
         nombre: "Combo x2",
         precio: 12500,
-        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at tincidunt arcu. Ut quis dolor leo. Sed id malesuada massa. Suspendisse aliquet faucibus commodo.",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         imagenDestacada: "",
         categoria: {
             id: "combos",
@@ -27,7 +27,7 @@ const productos = [
         id: "combo03",
         nombre: "Combo x4",
         precio: 12500,
-        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at tincidunt arcu. Ut quis dolor leo. Sed id malesuada massa. Suspendisse aliquet faucibus commodo.",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         imagenDestacada: "",
         categoria: {
             id: "combos",
@@ -38,7 +38,7 @@ const productos = [
         id: "hamburguesa01",
         nombre: "Hamburguesa 01",
         precio: 12500,
-        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at tincidunt arcu. Ut quis dolor leo. Sed id malesuada massa. Suspendisse aliquet faucibus commodo.",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         imagenDestacada: "",
         categoria: {
             id: "para-comer",
@@ -49,7 +49,7 @@ const productos = [
         id: "papas-fritas01",
         nombre: "Papas Fritas",
         precio: 12500,
-        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at tincidunt arcu. Ut quis dolor leo. Sed id malesuada massa. Suspendisse aliquet faucibus commodo.",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         imagenDestacada: "",
         categoria: {
             id: "para-picar",
@@ -60,7 +60,7 @@ const productos = [
         id: "bebida01",
         nombre: "Gaseosa 01",
         precio: 12500,
-        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at tincidunt arcu. Ut quis dolor leo. Sed id malesuada massa. Suspendisse aliquet faucibus commodo.",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         imagenDestacada: "",
         categoria: {
             id: "para-tomar",
@@ -71,33 +71,165 @@ const productos = [
 ]
 
 const gridProductos = document.querySelector("#grilla-productos");
+const btnCategorias = document.querySelectorAll(".btn-categoria");
+const tituloBanner = document.querySelector("#titulo-banner");
+const numeritoCarrito = document.querySelector("#numerito-carrito")
 
-function cargarProductos() {
-    productos.forEach(producto => {
-        let div = document.createElement("div");
-        div = classList.add("producto");
-        div.innerHTML = `
+let botonAgregarCarrito = document.querySelectorAll(".btn-agregar-al-carrito");
+
+
+
+
+function cargarProductos(productosSeleccionados) {
+
+    if (window.location.pathname === "/index.html") {
+
+        gridProductos.innerHTML = "";
+
+        productosSeleccionados.forEach(producto => {
+            let div = document.createElement("div");
+            div.classList.add("producto");
+            div.innerHTML = `
             <div class="producto-img">
                 <div class="img-destacada-producto">
-                    <img src="${productos.imagenDestacada}" alt="${productos.nombre}">
+                    <img src="${producto.imagenDestacada}" alt="${producto.nombre}">
                 </div>
             </div>
-            <div class="titulo-producto">${productos.nombre}</div>
-            <div class="subtitulo-producto">${productos.descripcion}</div>
+            <div class="titulo-producto">${producto.nombre}</div>
+            <div class="subtitulo-producto">${producto.descripcion}</div>
             <hr class="divisor-producto">
             <div class="footer-producto">
-            <div class="precio-producto"><span>$</span> ${productos.precio}</div>
-            <button class="btn-agregar-al-carrito" id="${productos.id}">
-            agregar al carrito
+            <div class="precio-producto"><span>$</span> ${producto.precio}</div>
+            <button class="btn-agregar-al-carrito" id="${producto.id}">
+            <i class="bi bi-cart-plus-fill"></i>
             </button>
             </div>
         
         `;
-        gridProductos.append(div);
+            gridProductos.append(div);
 
-    })
+        })
+
+
+
+    }
+    actualizarBtnCarrito();
 }
 
-cargarProductos();
-alert("Este es un mensaje de alerta.");
-// 
+cargarProductos(productos);
+
+btnCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        btnCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active")
+
+        if (e.currentTarget.id != "todos") {
+
+            const productosFiltrados = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosFiltrados);
+
+            const titularBanner = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloBanner.innerText = titularBanner.categoria.nombreCategoria;
+
+        }
+        else {
+            cargarProductos(productos);
+            tituloBanner.innerText = "Todos Los Productos"
+        }
+
+
+    })
+})
+
+function actualizarBtnCarrito() {
+    botonAgregarCarrito = document.querySelectorAll(".btn-agregar-al-carrito");
+
+    botonAgregarCarrito.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    })
+};
+
+let productosEnCarrito = []
+
+function agregarAlCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if (productosEnCarrito.some(producto => producto.id === idBoton)) {
+
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad += 1;
+
+    } else {
+        const nuevoProducto = { ...productoAgregado, cantidad: 1 };
+        productosEnCarrito.push(nuevoProducto);
+
+    }
+
+
+    actualizarNumeroCarrito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+
+}
+
+
+let numeroCarrito = 0
+
+function actualizarNumeroCarrito() {
+    numeroCarrito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numeritoCarrito.innerText = numeroCarrito;
+
+}
+
+let productosAgregadosAlCarrito = []
+
+
+
+let contenedorCarrito = document.querySelector("#contenedor-grilla-carrito");
+
+
+function paginaCarrito() {
+    if (window.location.pathname === "/html/carrito.html") {
+        productosAgregadosAlCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
+
+        if (productosAgregadosAlCarrito) {
+            contenedorCarrito.innerHTML = `
+                            <div class="contenedor-titulo-item-carrito">
+                    <div class="celda s"></div>
+                    <div class="celda l">Titulo</div>
+                    <div class="cantidad-en-carrito celda m">Cantidad</div>
+                    <div class="precio-en-carrito celda l">Precio</div>
+                    <div class="celda s"></div>
+                </div>
+            `
+            productosAgregadosAlCarrito.forEach(producto => {
+                console.log(producto.nombre)
+                let itemCarrito = document.createElement("div")
+                itemCarrito.classList.add("item-carrito");
+                itemCarrito.innerHTML = `
+                <div class="celda s"><img src="${producto.imagen}" alt="${producto.nombre}"></div>
+                <div class="titulo-en-carrito celda l">${producto.nombre}</div>
+                <div class="cantidad-en-carrito celda m">${producto.cantidad}</div>
+                <div class="precio-en-carrito celda l"><span>$</span> ${producto.precio * producto.cantidad}</div>
+                <div class="eliminar-del-carrito celda s" "><button id="${producto.id}"><i class="bi bi-trash-fill"></i></button></div>
+            `;
+
+                contenedorCarrito.append(itemCarrito);
+
+            })
+
+
+            console.log("si hay");
+        } else {
+            contenedorCarrito.innerHTML = `
+       <div class="No hay productos en el carrito"><span>No hay productos en el carrito</span></div>
+        <div class="div-volver-a-la-tienda"><a href="../index.html">Volver a la tienda</a></div>
+       `
+        }
+
+    }
+}
+
+
+paginaCarrito();
